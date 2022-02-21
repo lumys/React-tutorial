6a8c3ad512bfad832bef3f2cfc0df3f9d92d07f8
김영환 <gim-yeonghwan@gim-yeonghwan-ui-M1-Pro-MacBookPro.local>
2022-02-21 12:40:37 +0900

    Route 라이브러리 추가
    - 기본 tutorial 코드 추가
    - atomic 디자인 구성으로 정리

---
+++ src/pages/About/index.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Templates from "./templates";
+
+const About = () => {
+  return <Templates />;
+}
+
+export default About;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Templates from "./templates";
+
+const Home = () => {
+  return <Templates />;
+}
+
+export default Home;
