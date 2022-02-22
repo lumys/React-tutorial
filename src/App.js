f996b98aad72508aaff790d0c07157f5d820c3bc
김영환 <gim-yeonghwan@gim-yeonghwan-ui-M1-Pro-MacBookPro.local>
2022-02-22 11:08:44 +0900

    ie11 지원 추가 등
    ie11 지원 라이브러리 설정 추가
    일부 prettier 미적용 코드 적용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from "./pages/Home"
-import About from "./pages/About"
+import Home from './pages/Home';
+import About from './pages/About';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </div>
-  );
-}
+const App = () => (
+  <div className='App'>
+    <h1>Welcome to React Router!</h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='about' element={<About />} />
+    </Routes>
+  </div>
+);
 
 export default App;
